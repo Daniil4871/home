f6c86997c6e7999273292c9fddafa50ec82ccfd2
Даниил Павлович <danya.vshivtsev@mil.ru>
2018-09-22 01:01:57 +0500

    Test commit

--- src/home.java
+++ src/home.java
@@ -7,21 +7,21 @@ public class home {
     public static void main(String[] args) {
         {
             long d = 1, result = 0, number2;
-            int number;
+            long number;
 
             System.out.println("Enter number");
             Scanner scanner = new Scanner(System.in);
-            number = scanner.nextInt();
+            number = scanner.nextLong();
             number2 = number;
             char end;
-            if ((number2 >= -128) && (number2 <= 127)) System.out.println("It's byte type");
-            if ((number2 >= -32768) && (number2 <= 32767)) System.out.println("It's short type");
-            if ((number2 >= -2147483648) && (number2 <= 2147483647)) System.out.println("It's integer type");
-             System.out.println("It's long type");
+            if ((number2 >= Byte.MIN_VALUE) && (number2 <= Byte.MAX_VALUE)) System.out.println("It can be byte type");
+            if ((number2 >= Short.MIN_VALUE) && (number2 <= Short.MAX_VALUE)) System.out.println("It can be short type");
+            if ((number2 >= Integer.MIN_VALUE) && (number2 <= Integer.MAX_VALUE)) System.out.println("It can be integer type");
+            if ((number2 >= Long.MIN_VALUE) && (number2 <= Long.MAX_VALUE)) System.out.println("It can be long type");
 
 
             while (number >= 1) {
-                if (number % 2 != 0) result = result + (d);
+                if (number % 2 != 0) result = result + d;
                 d = d * 10;
                 number /= 2;
             }
